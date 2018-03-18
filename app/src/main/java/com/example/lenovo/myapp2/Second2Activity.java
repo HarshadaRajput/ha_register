1ba4c1300883175376f3934ff0b1a5e09309341a
Girish M <girishmane8692@gmail.com>
2018-03-18 15:20:41 +0530

    - code optimization

--- app/src/main/java/com/example/lenovo/myapp2/Second2Activity.java
+++ app/src/main/java/com/example/lenovo/myapp2/Second2Activity.java
@@ -2,9 +2,10 @@ package com.example.lenovo.myapp2;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -13,8 +14,12 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class Second2Activity extends AppCompatActivity {
+public class Second2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean recordOn = false;
+
     private TextView resultTEXT;
+    private AppCompatImageView recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,11 @@ public class Second2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_second2);
         resultTEXT = (TextView) findViewById(R.id.TVresult2);
 
+        recordButton = (AppCompatImageView) findViewById(R.id.imageButton);
+        recordButton.setOnClickListener(this);
     }
 
-    public void onButtonClick(View v) {
+/*    public void onButtonClick(View v) {
         if (v.getId() == R.id.imageButton) {
 
             promptSpeechInput();
@@ -33,9 +40,23 @@ public class Second2Activity extends AppCompatActivity {
         }
 
 
+    }*/
+
+    private void toggleRecord() {
+        if (recordOn) {
+            recordOn = false;
+            recordButton.setImageResource(R.drawable.ic_recording_off);
+        } else {
+            recordOn = true;
+            recordButton.setImageResource(R.drawable.ic_recording_on);
+        }
     }
 
     public void promptSpeechInput() {
+        if (recordOn) {
+            Toast.makeText(Second2Activity.this, "Already Recording....", Toast.LENGTH_LONG).show();
+            return;
+        }
         Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
@@ -43,13 +64,13 @@ public class Second2Activity extends AppCompatActivity {
 
         try {
             startActivityForResult(i, 100);
+            toggleRecord();
         } catch (ActivityNotFoundException a) {
             Toast.makeText(Second2Activity.this, "Sorry! your device doesn't support speech language", Toast.LENGTH_LONG).show();
-
         }
     }
 
-    private String output(String word){
+    private String output(String word) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Word: ");
         stringBuilder.append(word);
@@ -61,15 +82,13 @@ public class Second2Activity extends AppCompatActivity {
         int code = Dictionary.getCode(MakeCode.getCode(word));
         stringBuilder.append(code);
         stringBuilder.append("\n");
-        if(code <= 0) {
+        if (code <= 0) {
             stringBuilder.append("Invalid Voice Command");
-        }else {
+        } else {
             stringBuilder.append("Valid Voice Command");
         }
         return stringBuilder.toString();
-        }
-
-
+    }
 
 
     @Override
@@ -79,11 +98,19 @@ public class Second2Activity extends AppCompatActivity {
             case 100:
                 if (resultCode == RESULT_OK && i != null) {
                     ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    Log.e(Second2Activity.class.getSimpleName(),"result: " +result);
+                    Log.e(Second2Activity.class.getSimpleName(), "result: " + result);
                     resultTEXT.setText(output(result.get(0)));
-
-
+                    toggleRecord();
                 }
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton:
+                promptSpeechInput();
                 break;
         }
     }

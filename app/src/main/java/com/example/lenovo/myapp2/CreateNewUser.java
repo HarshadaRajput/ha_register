15fb67a2817e498c1ddc5ba8c5c7da643c140731
Girish <gmane@birdzi.com>
2018-04-14 18:11:01 +0530

    - unused file removed

--- app/src/main/java/com/example/lenovo/myapp2/CreateNewUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.myapp2;
-
-/**
- * Created by lenovo on 01-Apr-18.
- */
-
-interface CreateNewUser {
-    String doInBackground(String... strings);
-}
--- app/src/main/java/com/example/lenovo/myapp2/JSONParser.java
+++
@@ -1,8 +0,0 @@
-package com.example.lenovo.myapp2;
-
-/**
- * Created by lenovo on 13-Mar-18.
- */
-
-class JSONParser {
-}
